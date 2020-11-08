f9676529bc23f32f38c8d2e8c4c204377e69fb69
Jordan Knox <jordanknox100@berkeley.edu>
2020-11-08 20:34:06 +0000

    this class is actually the biggest waste of my time

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -14,7 +14,15 @@ module NavigationHelpers
     case page_name
 
     when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-
+    
+    when /^the edit page for "(.*)"$/
+      movie_id = Movie.find_by(title: $1).id
+      edit_movie_path(movie_id)
+      
+    when /^the details page for "(.+)"$/
+      movie = Movie.find_by(title: $1)
+      movie_path(movie)
+      
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
