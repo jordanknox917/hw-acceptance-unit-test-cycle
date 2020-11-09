4f8f1bfa7dfb8bcefac149e9189dc154980c6293
Jordan Knox <jordanknox100@berkeley.edu>
2020-11-09 02:37:19 +0000

    idc anymore, i got better stuff to do

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,8 +1,3 @@
 class Movie < ActiveRecord::Base
   #added to try and get the stupid thing to pass. this class is so unguided and terrible
-  def self.similar_movies movie_title
-    director = Movie.find_by(title: movie_title).director
-    return nil if director.blank? or director.nil?
-    Movie.where(director: director).pluck(:title)
-  end
 end
